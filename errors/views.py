cf3b6a6830ae289edfbd6abc93752c867958b166
Brett O'Connor <brett@blimpsgo90.com>
2016-11-19 09:51:30 -0700

    random error art

--- errors/views.py
+++ errors/views.py
@@ -9,7 +9,8 @@ def display(request, slug):
     try:
         error_art = ErrorArt.objects.get(slug=slug)
     except:
-        return HttpResponse('Didnt find that page so show a random ErrorArt here!',status=404)
+        random_error_art = ErrorArt.objects.order_by('?')[0]
+        return HttpResponse(random_error_art.content,status=404)
     # render raw content of object found!
     return HttpResponse(error_art.content,status=404)
 
