d05176fbf9d19c943d1d7b1dec2ddeae6fc1801c
Brett O'Connor <brett@blimpsgo90.com>
2016-11-19 15:34:54 -0700

    got static files sorted

--- errors/models.py
+++ errors/models.py
@@ -7,6 +7,6 @@ class ErrorArt(models.Model):
     artist = models.CharField(max_length=200,help_text="Artist name. Required.",blank=False)
     artist_url = models.URLField(help_text="Artist homepage or whatever. Not required.",blank=True)
     slug = models.SlugField(help_text="The part of the URL we want to be able to link this 404 error from, without the slash. Ex: A value of 'my-404-page' would resolve to http://404error.gallery/my-404-page. Required.",blank=False)
-    content = models.TextField(help_text="The ENTIRE HTML for the page. We'll figure out static assets later. Required.",blank=False)
+    content = models.TextField(help_text="The ENTIRE HTML for the page. You'll need to add static assets to the appropriate errors/static/* folder and reference them like src=\"/static/errors/img/[asset].gif\" Required.",blank=False)
     def __str__(self):
         return self.artist
